900bc77d27687c6a80a67e490be30b06d0e101b7
liton-cse <litonakash13@gmail.com>
2026-02-19 03:26:42 +0600

    ghgh

--- src/app/modules/coachPanel/ped/ped.route.ts
+++ src/app/modules/coachPanel/ped/ped.route.ts
@@ -29,7 +29,8 @@ router.patch(
  */
 
 // Get all PEDs for logged-in athlete (with optional week filter)
-router.get('/app-data', auth(USER_ROLES.ATHLETE), controller.getPEDForAthleteInApp);
+router.get('/app-data', auth(USER_ROLES.ATHLETE, USER_ROLES.COACH
+), controller.getPEDForAthleteInApp);
 router.get('/', auth(USER_ROLES.COACH, USER_ROLES.SUPER_ADMIN), controller.getPEDByAthlete);
 
 // Optional: Get PED by week via query param
